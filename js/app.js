cee82a8dff315055e375674174dc62c5935170d6
JON STILLSON <JCSTILLSON77@GMAIL.COM>
2023-05-18 18:06:56 -0400

    redo

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 function AppState() {
   this.allProducts = [];
-}
+};
 
 AppState.prototype.instantiateProducts = function () {
 
@@ -16,7 +16,7 @@ AppState.prototype.instantiateProducts = function () {
     }
   }
 
-}
+};
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
@@ -26,8 +26,38 @@ AppState.prototype.saveToLocalStorage = function () {
 
     //Step 2 - Store the stringy data to local storage
   localStorage.setItem('myImages', stringyImages);
-}
+};
 
+AppState.prototype.loadItems = function () {
+  // Step 1: Get data from local storage
+  let retrievedImages = localStorage.getItem('myImages');
+  console.log('Images from local storage >>> ', retrievedImages);
+
+  // Step 2: Convert the retrieved data into usable code
+  let parsedImages = JSON.parse(retrievedImages);
+  console.log('Parsed images >>> ', parsedImages);
+
+  // Step 3: Clear the existing products in allProducts array
+  this.allProducts = [];
+
+  // Step 4: Reconstruct loaded data or instantiate new products
+  if (parsedImages) {
+    for (let i = 0; i < parsedImages.length; i++) {
+      const productData = parsedImages[i];
+      const product = new Product(productData.name);
+
+      product.timesClicked = productData.timesClicked;
+      product.timesShown = productData.timesShown;
+
+      this.allProducts.push(product);
+    }
+  } else {
+    // If no data is found in local storage, instantiate new products
+    this.instantiateProducts();
+  }
+};
+
+/*
 AppState.prototype.loadItems = function () {
 
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
@@ -39,31 +69,31 @@ AppState.prototype.loadItems = function () {
   let parsedImages = JSON.parse(retrievedImages);
   console.log('Parsed images >>> ', parsedImages);
 
-    //step 3 reconstructing loaded data OR instantiating new products
-  //if(retrievedImages){
-    for(let i = 0; i < parsedImages.length; i++){
-      if(parsedImages[i].name === 'sweep'){
-        let reconstructedSweep = new Product(parsedImages[i].name, 'png');
-        reconstructedSweep.views = parsedImages[i].view;
-        reconstructedSweep.votes = parsedImages[i].votes;
-        productIndexArray.push(reconstructedSweep); 
-      } else {
-        let reconstructedImage = new Product(parsedImages[i].name);
-        reconstructedImage.views = parsedImages[i].views;
-        reconstructedImage.votes = parsedImages[i].views; 
-        productIndexArray.push(reconstructedImage);
-      }
-      
-    this.instantiateProducts();
-  }
+  //step 3 reconstructing loaded data OR instantiating new products
+  
 }
-  //}
-    
-//}
+*/
+
+/*
+if(retrievedImages){
+  for(let i = 0; i < parsedImages.length; i++){
+    if(parsedImages[i].name === 'sweep'){
+      let reconstructedSweep = new Product(parsedImages[i].name, 'png');
+      reconstructedSweep.views = parsedImages[i].view;
+      reconstructedSweep.votes = parsedImages[i].votes;
+      productIndexArray.push(reconstructedSweep); 
+    } else {
+      let reconstructedImage = new Product(parsedImages[i].name);
+      reconstructedImage.views = parsedImages[i].views;
+      reconstructedImage.votes = parsedImages[i].views; 
+      productIndexArray.push(reconstructedImage);
+    }
+  }
+} */
 
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
   this.source = `assets/${name}.${fileExtension}`;
   this.timesClicked = 0;
   this.timesShown = 0;
-}
+};
