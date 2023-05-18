5d2c183038c1214003f0a0e2d0b6d271479ab377
Gerald Bruno <34822464+NephilimEyez@users.noreply.github.com>
2023-05-18 19:40:45 -0400

    chart

--- js/chart.js
+++ js/chart.js
@@ -11,6 +11,51 @@ let canvasElem = document.getElementById('chart')
  *
  */
 function renderChart() {
+
+    let dataSets = localStorage.getItem('products');
+    let cleanData = JSON.parse(dataSets);
+    console.log(cleanData);
+
+    let itemNames = [];
+    let itemVotes = [];
+    let itemViews = [];
+    
+    for (let i = 0; i < cleanData.length; i++) {
+        itemNames.push(cleanData[i].name);
+        itemVotes.push(cleanData[i].timesClicked);
+        itemViews.push(cleanData[i].timesShown);
+    }
+    const data = {
+        labels: itemNames,
+        datasets: [{
+            label: 'Votes',
+            data: itemVotes,
+            backgroundColor: '#fff',
+            borderColor: '#fff',
+            borderWidth: 1
+        },
+        {
+            label: 'Views',
+            data: itemViews,
+            backgroundColor: '#7e57c2',
+            borderColor: '$7e57c2',
+            borderWidth: 1
+        }]
+    };
+    
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        },
+    };
+    const myGraph = new Chart(canvasElem, config);
+
 }
 
 renderChart();
